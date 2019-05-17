d15c62fe046b1320a32ec0d129dcbf4b1af25edf
jihoon289 <jihoon289@naver.com>
2019-05-17 10:41:50 +0900

    Update BasicTestHandler.cs

--- BasicTestHandler.cs
+++ BasicTestHandler.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using YuantaCOMLib;
-using System.Threading;
 
 
 namespace YOACOMClientCSharp
@@ -13,17 +12,15 @@ namespace YOACOMClientCSharp
     {
         public MainForm m_mainForm;
         public IYuantaAPI m_iYuantaAPI;
-        public Dictionary<int, string> m_mapRequestTR;
-        public Dictionary<int, string> m_mapAutoTR;
-        private string m_strJongCode;
+        public Dictionary<int, string> m_mapRequestTR = new Dictionary<int, string>();
+        public Dictionary<int, string> m_mapAutoTR = new Dictionary<int, string>();
+        public string m_strJongCode;
 
         public BasicTestHandler()
         {
-            m_mainForm      = null;
-            m_iYuantaAPI    = null;
+            m_mainForm = null;
+            m_iYuantaAPI = null;
 
-            m_mapRequestTR = new Dictionary<int, string>();
-            m_mapAutoTR = new Dictionary<int, string>();
 
             m_strJongCode = "";
         }
@@ -32,75 +29,27 @@ namespace YOACOMClientCSharp
         {
             m_strJongCode = m_mainForm.txtBTJongCode.Text;
             m_strJongCode.Trim();
+            int nErr = m_iYuantaAPI.YOA_SetTRInfo("300001", "InBlock1");
+            nErr = m_iYuantaAPI.YOA_SetFieldString("jang", "1", 0);
+            nErr = m_iYuantaAPI.YOA_SetFieldString("jongcode", m_strJongCode, 0);   // i로 수정함
+            nErr = m_iYuantaAPI.YOA_SetFieldString("outflag", "N", 0);
+            nErr = m_iYuantaAPI.YOA_SetFieldString("tsflag", "0", 0);
 
-            //추가부분 
-            string[] jongCodeList = new string[] { "005930", "105560", "207940", "180640" };
-            
+            int nReqID = m_iYuantaAPI.YOA_Request("300001", true, -1);
 
-            foreach (string i in jongCodeList)
+            if (CommDef.ERROR_MAX_CODE < nReqID)
             {
-                int nErr = m_iYuantaAPI.YOA_SetTRInfo("300001", "InBlock1");
-                //추가부분 
-                //MessageBox.Show("nErr: " + nErr);       // 202 DSO를 찾을수없음
-
-                if (CommDef.ERROR_MAX_CODE > nErr)
-                {
-                    string strMsg = m_iYuantaAPI.YOA_GetErrorMessage(nErr);     // 조회 요청 실패에 대한 메시지를 얻을 수 있습니다.
-                    m_mainForm.lbLog.Items.Insert(0, strMsg);
-                }
-
-                nErr = m_iYuantaAPI.YOA_SetFieldString("jang", "1", 0);
-                if (CommDef.ERROR_MAX_CODE > nErr)
-                {
-                    string strMsg = m_iYuantaAPI.YOA_GetErrorMessage(nErr);     // 조회 요청 실패에 대한 메시지를 얻을 수 있습니다.
-                    m_mainForm.lbLog.Items.Insert(0, strMsg);
-                }
-
-                nErr = m_iYuantaAPI.YOA_SetFieldString("jongcode", i, 0);   //m_strJongCode를 i로 수정함
-                if (CommDef.ERROR_MAX_CODE > nErr)
-                {
-                    string strMsg = m_iYuantaAPI.YOA_GetErrorMessage(nErr);     // 조회 요청 실패에 대한 메시지를 얻을 수 있습니다.
-                    m_mainForm.lbLog.Items.Insert(0, strMsg);
-                }
-
-                nErr = m_iYuantaAPI.YOA_SetFieldString("outflag", "N", 0);
-                if (CommDef.ERROR_MAX_CODE > nErr)
-                {
-                    string strMsg = m_iYuantaAPI.YOA_GetErrorMessage(nErr);     // 조회 요청 실패에 대한 메시지를 얻을 수 있습니다.
-                    m_mainForm.lbLog.Items.Insert(0, strMsg);
-                }
-
-                nErr = m_iYuantaAPI.YOA_SetFieldString("tsflag", "0", 0);
-                if (CommDef.ERROR_MAX_CODE > nErr)
-                {
-                    string strMsg = m_iYuantaAPI.YOA_GetErrorMessage(nErr);     // 조회 요청 실패에 대한 메시지를 얻을 수 있습니다.
-                    m_mainForm.lbLog.Items.Insert(0, strMsg);
-                }
-
-                int nReqID = m_iYuantaAPI.YOA_Request("300001", true, -1); // 조회 요청 성공 시, ERROR_MAX_CODE(1000)보다 큰 수의 고유 요청ID 반환합니다.
-                                                                           // 조회응답을 받았을, 구분하는 ID로 사용합니다.
-
-                //추가부분 
-                //MessageBox.Show("nReqID : " + nReqID);  // -1 알수없는 오류
-
-                if (CommDef.ERROR_MAX_CODE < nReqID)
-                {
-                    m_mapRequestTR[nReqID] = "30001";
-                    m_mainForm.lbLog.Items.Insert(0, "[300001]주식현재가 조회를 요청하였습니다.");
-                }
-                else               // 조회 요청 실패 시, 오류 처리
-                {
-                    string strMsg = m_iYuantaAPI.YOA_GetErrorMessage(nReqID);     // 조회 요청 실패에 대한 메시지를 얻을 수 있습니다.
-                    m_mainForm.lbLog.Items.Insert(0, strMsg);
-                }
-
+                m_mapRequestTR[nReqID] = "300001";
+                m_mainForm.lbLog.Items.Insert(0, "[300001]주식현재가 조회를 요청하였습니다.");
             }
+            //Console.WriteLine("1");
+
         }
         public void RegistAuto()
         {
-            UnRegistAuto();
-            
-            m_iYuantaAPI.YOA_SetTRFieldString("11", "InBlock1", "jongcode", m_mainForm.txtBTJongCode.Text, 0);      //m_mainForm.txtBTJongCode.Text를 i로 바꿈
+            // UnRegistAuto();
+
+            m_iYuantaAPI.YOA_SetTRFieldString("11", "InBlock1", "jongcode", m_strJongCode, 0);      //m_mainForm.txtBTJongCode.Text를 i로 바꿈
             int nReqID = m_iYuantaAPI.YOA_RegistAuto("11");
 
             if (CommDef.ERROR_MAX_CODE < nReqID)
@@ -110,16 +59,13 @@ namespace YOACOMClientCSharp
                 m_mainForm.btnBTAutoUnRegist.Enabled = true;
 
                 m_mainForm.lbLog.Items.Insert(0, "[11]주식 실시간체결 Auto가 등록 되었습니다.");
-            }
-            else
-            {
-                string strMsg = m_iYuantaAPI.YOA_GetErrorMessage(nReqID);     // 실시간 등록 실패에 대한 메시지를 얻을 수 있습니다.
-                m_mainForm.lbLog.Items.Add(strMsg);
+                //Console.WriteLine("3");
+
             }
         }
         public void UnRegistAuto()
         {
-            foreach(KeyValuePair<int, string> autoTR in m_mapAutoTR)
+            foreach (KeyValuePair<int, string> autoTR in m_mapAutoTR)
             {
                 int nResult = m_iYuantaAPI.YOA_UnRegistAutoWithReqID(autoTR.Key);
 
@@ -156,7 +102,7 @@ namespace YOACOMClientCSharp
             }
             else if (nErrCode == CommDef.ERROR_REQUEST_FAIL)
             {
-                m_mainForm.lbLog.Items.Insert(0, strName + m_iYuantaAPI.YOA_GetErrorMessage(nErrCode));
+                //m_mainForm.lbLog.Items.Insert(0, strName + m_iYuantaAPI.YOA_GetErrorMessage(nErrCode));       // 많은 종목에 대해서 최초수신시에 dso 요청실패 오류가 뜸 - 잠시 주석처리
             }
         }
 
@@ -165,23 +111,16 @@ namespace YOACOMClientCSharp
             string strTRID = m_mapRequestTR[nReqID];
             if (-1 != strTRID.CompareTo("300001"))
             {
-                m_mainForm.txtBTJongName.Text = m_iYuantaAPI.YOA_GetTRFieldString("300001", "OutBlock1", "jongname", 0);
+                m_mainForm.txtBTJongName.Text = m_iYuantaAPI.YOA_GetTRFieldString("300001", "OutBlock1", "jongname", 0);    // GetTRFieldString 은 set,get동시
                 m_mainForm.txtBTCurJuka.Text = m_iYuantaAPI.YOA_GetTRFieldString("300001", "OutBlock1", "curjuka", 0);
-
-                m_iYuantaAPI.YOA_SetTRInfo("300001", "OutBlock1");
+                m_iYuantaAPI.YOA_SetTRInfo("300001", "OutBlock1");  // SetTRInfo -> GetFieldString 값 접근
                 m_mainForm.txtBTDebi.Text = m_iYuantaAPI.YOA_GetFieldString("debi", 0);
                 m_mainForm.txtBTDebiRate.Text = m_iYuantaAPI.YOA_GetFieldString("debirate", 0);
                 m_mainForm.txtBTVolume.Text = m_iYuantaAPI.YOA_GetFieldString("volume", 0);
 
                 m_mainForm.lbLog.Items.Insert(0, "[300001]주식현재가 응답을 수신하였습니다.");
+                //Console.WriteLine("2");
 
-                // 추가부분 - 메인 로그에 정보찍기
-                m_mainForm.LogMessage("\t종목명 :" + m_iYuantaAPI.YOA_GetTRFieldString("300001", "OutBlock1", "jongname", 0));       // 종목명
-                m_mainForm.LogMessage("\t현재가 :" + m_iYuantaAPI.YOA_GetTRFieldString("300001", "OutBlock1", "curjuka", 0));       // 현재가
-                m_mainForm.LogMessage("\t전일대비 :" + m_iYuantaAPI.YOA_GetFieldString("debi", 0));      // 전일대비
-                m_mainForm.LogMessage("\t등락률 :" + m_iYuantaAPI.YOA_GetFieldString("debirate", 0));    // 등락률
-                m_mainForm.LogMessage("\t거래량 :" + m_iYuantaAPI.YOA_GetFieldString("volume", 0));      // 거래량
-                m_mainForm.LogMessage("------------------------------------------");                     // 구분선
             }
         }
 
@@ -192,23 +131,23 @@ namespace YOACOMClientCSharp
                 string strOutCode = m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "jongcode", 0);
                 strOutCode.Trim();
 
-                if (m_strJongCode.Equals(strOutCode))
-                {
+                if (MainForm.jongCodeList.Contains(m_strJongCode))
+                {/*
                     //int nCurJuka = m_iYuantaAPI.YOA_GetTRFieldLong("11", "OutBlock1", "curjuka", 0);
                     m_mainForm.txtBTCurJuka.Text = m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "curjuka", 0);
                     m_mainForm.txtBTDebi.Text = m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "debi", 0);
                     m_mainForm.txtBTDebiRate.Text = m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "debirate", 0);
                     m_mainForm.txtBTVolume.Text = m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "volume", 0);
-
-                    /*
-                    // 추가부분 - 메인 로그에 정보찍기
-                    m_mainForm.LogMessage("\t종목명 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "jongcode", 0));       // 종목명
-                    m_mainForm.LogMessage("\t현재가 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "curjuka", 0));       // 현재가
-                    m_mainForm.LogMessage("\t전일대비 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "debi", 0));      // 전일대비
-                    m_mainForm.LogMessage("\t등락률 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "debirate", 0));    // 등락률
-                    m_mainForm.LogMessage("\t거래량 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "volume", 0));      // 거래량
-                    m_mainForm.LogMessage("------------------------------------------");                     // 구분선
                     */
+                    
+                    // 추가부분 - 메인 로그에 정보찍기
+                    Console.WriteLine("종목코드 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "jongcode", 0)+"------------------");       // 종목명
+                    Console.WriteLine("현재가 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "curjuka", 0));       // 현재가
+                    Console.WriteLine("전일대비 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "debi", 0));      // 전일대비
+                    Console.WriteLine("등락률 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "debirate", 0));    // 등락률
+                    Console.WriteLine("거래량 :" + m_iYuantaAPI.YOA_GetTRFieldString("11", "OutBlock1", "volume", 0));      // 거래량
+                    //Console.WriteLine("4");
+
                 }
             }
         }
