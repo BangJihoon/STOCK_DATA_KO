822e4b962e1445cf43f0ba3e2162359827bbf002
jihoon289 <jihoon289@naver.com>
2019-05-23 15:25:13 +0900

    Add files via upload

---
+++ insert_info.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YOACOMClientCSharp
+{
+    class insert_info
+    {
+        public ObjectId Id { get; set; }
+
+        public object Jongcode { get; set; }
+
+        public object Curjuka { get; set; }
+
+        public object Debi { get; set; }
+
+        public object Debirate { get; set; }
+
+        public object Volume { get; set; }
+        // 추가 
+
+        public override string ToString()
+        {
+            return Jongcode + " " + Curjuka + "" + Debi + "" + Debirate + "" + Volume;//  추가되면 뒤에 변수 + " "붙여 줄것
+        }
+    }
+}
